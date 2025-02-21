eca1858f5843b42255bdfcae402c412087823068
Tautvydas <tassat12321@gmail.com>
2025-02-21 14:12:20 +0200

    Filmų šalinimas

--- views/interface.py
+++ views/interface.py
@@ -11,27 +11,13 @@ def sukti_menu(filmai):
         try:
             veiksmas = input("Iveskite veiksma: \n")
             if veiksmas == "1":  # Prideti filma #TODO: iskelti i filmas_service?
-                filmas = data_handler.ivesti_filma()
-                filmai.append(filmas)
-                print("Filmas sekmingai pridetas!")
-                data_handler.issaugoti_i_faila(filmu_failas, filmai)
+                prideti_filma(filmai)
             elif veiksmas == "2":  # Perziureti filmus
-                for filmas in filmai:
-                    print(filmas)
+                atspausdinti_filmus(filmai)
             elif veiksmas == '3': # Filmu paieska
-                paieska = input("Iveskite, ko ieskoma: \n")
-                paieska_tipas = input("Ar norite ieskoti pagal pavadinima ('p') ar zanra ('z')? \n")
-                if paieska_tipas == "p":
-                    rasti = filmu_paieska_pavadinimas(filmai, paieska.lower())
-                elif paieska_tipas == "z":
-                    rasti = filmu_paieska_zanras(filmai, paieska.lower())
-                else:
-                    continue
-                if len(rasti) == 0:
-                    print("Nieko neradome!")
-                else:
-                    for filmas in filmai:
-                        print(filmas)
+                ieskoti_filmu(filmai)
+            elif veiksmas == '4': # Filmu salinimas
+                pasalinti_filma(filmai)
             elif veiksmas == "":
                 return
             else:
@@ -39,3 +25,43 @@ def sukti_menu(filmai):
         except Exception as e:
             print("Ivyko klaida!")
             print(e)
+
+def prideti_filma(filmai):
+    filmas = data_handler.ivesti_filma()
+    filmai.append(filmas)
+    print("Filmas sekmingai pridetas!")
+    data_handler.issaugoti_i_faila(filmu_failas, filmai)
+
+def atspausdinti_filmus(filmai):
+    for filmas in filmai:
+        print(filmas)
+
+def ieskoti_filmu(filmai):
+        paieska = input("Iveskite, ko ieskoma: \n")
+        paieska_tipas = input("Ar norite ieskoti pagal pavadinima ('p') ar zanra ('z')? \n")
+        if paieska_tipas == "p":
+            rasti = filmu_paieska_pavadinimas(filmai, paieska.lower())
+        elif paieska_tipas == "z":
+            rasti = filmu_paieska_zanras(filmai, paieska.lower())
+        else:
+            print("Neteisingai ivestas paieskos tipas!")
+            return
+        if len(rasti) == 0:
+            print("Nieko neradome!")
+        else:
+            atspausdinti_filmus(rasti)
+
+def pasalinti_filma(filmai):
+    pavadinimas = input("Iveskite filmo pavadinima, kuri norite pasalinti: \n")
+    rastas = None
+    for filmas in filmai:
+        if filmas.pavadinimas == pavadinimas:
+            rastas = filmas
+            break
+    if rastas is None:
+        print("Tokio filmo nepavyko rasti! \n")
+        return
+    else:
+        filmai.remove(rastas)
+        print("Filmas sekmingai pasalintas!")
+        data_handler.issaugoti_i_faila(filmu_failas, filmai)
